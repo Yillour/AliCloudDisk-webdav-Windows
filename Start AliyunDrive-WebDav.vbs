50d151c01de09d2c75184e6b238ac16f72b3a530
析木 <38829937+Yillour@users.noreply.github.com>
2022-03-06 11:08:42 +0800

    Add files via upload

---
+++ Start AliyunDrive-WebDav.vbs
@@ -0,0 +1,2 @@
+set ws=WScript.CreateObject("WScript.Shell") 
+ws.Run "D:\Programs\aliyundrive-webdav\start.bat /start",0
---
+++ start.bat
@@ -0,0 +1,2 @@
+@echo off
+D:\Programs\aliyundrive-webdav\aliyundrive-webdav.exe --refresh-token 36508e9dd85c4860832f08fcf101860b
